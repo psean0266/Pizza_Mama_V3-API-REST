ccec3737cca6d5b4a1ebc106d244475f00d28a13
AbdoulayePangnonKOUL <AbdoulayePangnonKOUL@TAL-7G6S793>
2024-09-12 11:17:42 +0200

    User_management

--- pizza_mama/Pages/Admin/Index.cshtml.cs
+++ pizza_mama/Pages/Admin/Index.cshtml.cs
@@ -9,22 +9,33 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace pizza_mama.Pages.Admin
 {
     public class IndexModel : PageModel
     {
         public bool DisplayInvalidAccountMessage = false;
+        public bool IsDeveloppementMode = false;
         private readonly pizza_mama.Data.DataContext _context;
 
         IConfiguration configuration;
 
-        public IndexModel (IConfiguration configuration, pizza_mama.Data.DataContext context)
-        {   
+        public IndexModel(IConfiguration configuration, pizza_mama.Data.DataContext context, IWebHostEnvironment env)
+        {
             this.configuration = configuration;
-            this._context = context;    
+            this._context = context;
+
+
+            if (env.IsDevelopment())
+            {
+                IsDeveloppementMode = true;
+            }
         }
-        public IActionResult OnGet() 
+        public IActionResult OnGet()
         {
             if (HttpContext.User.Identity.IsAuthenticated)
             {
@@ -32,7 +43,7 @@ namespace pizza_mama.Pages.Admin
             }
             return Page();
         }
-        public async Task<IActionResult> OnPostAsync(string username, string password, string ReturnUrl) 
+        public async Task<IActionResult> OnPostAsync(string username, string password, string ReturnUrl)
         {
             var authSection = configuration.GetSection("Auth");
             string adminLogin = authSection["AdminLogin"];
@@ -57,17 +68,23 @@ namespace pizza_mama.Pages.Admin
             if (username != null)
             {
 
-                var user = await _context.Accounts.FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
+                //    //var user = await _context.Accounts.FirstOrDefaultAsync(u => u.Username == username && u.Password == password);
 
-                if (user != null)
+                   var user = await _context.Accounts.FirstOrDefaultAsync(u => u.Username == username); 
+
+                //    //   if (user != null && VerifyPassword(password, user.Password))
+
+                //    // if (user != null &&(password == user.Password))
+
+                 if (user != null && DecryptPassword(user.Password) == password)
                 {
 
                     DisplayInvalidAccountMessage = false;
                     var claims = new List<Claim>
-                    {
-                       new Claim(ClaimTypes.Name, user.Username),
-                       new Claim(ClaimTypes.Role, user.Roles)
-                    };
+                        {
+                           new Claim(ClaimTypes.Name, user.Username),
+                           new Claim(ClaimTypes.Role, user.Roles)
+                        };
                     var claimsIdentity = new ClaimsIdentity(claims, "Login");
                     await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new
                     ClaimsPrincipal(claimsIdentity));
@@ -79,7 +96,85 @@ namespace pizza_mama.Pages.Admin
             DisplayInvalidAccountMessage = true;
             return Page();
         }
-        
+
+
+        //private bool VerifyPassword(string inputPassword, string hashedPassword)
+        //{
+        //    // Vous devez implémenter la logique de vérification du mot de passe
+        //    using (var sha = SHA256.Create())
+        //    {
+        //        // Récupérer le sel depuis le hachage stocké (si votre modèle de données le permet)
+        //        byte[] salt = Convert.FromBase64String(hashedPassword.Split('$')[0]);
+
+        //        // Concaténer le mot de passe fourni avec le sel
+        //        byte[] combined = Encoding.UTF8.GetBytes(inputPassword + Convert.ToBase64String(salt));
+
+        //        // Calculez le nouveau hachage
+        //        byte[] hashedInputPassword = sha.ComputeHash(combined);
+
+        //        // Comparez les deux hachages
+        //       return hashedPassword.SequenceEqual(Convert.ToBase64String(hashedInputPassword));
+
+        //    }
+        //}
+
+        private bool VerifyPassword(string inputPassword, string storedPassword)
+        {
+            using (var sha = SHA256.Create())
+            {
+                // Split pour obtenir le sel stocké
+                string[] passwordParts = storedPassword.Split('$');
+                if (passwordParts.Length != 2)
+                {
+                    // Gérer une situation incorrecte (par exemple, le format de hachage est invalide)
+                    return false;
+                }
+
+                string storedSalt = passwordParts[0];
+                string storedHash = passwordParts[1];
+
+                // Convertir le sel stocké en tableau de bytes
+                byte[] salt = Convert.FromBase64String(storedSalt);
+
+                // Concaténer le mot de passe fourni avec le sel
+                byte[] combined = Encoding.UTF8.GetBytes(inputPassword + Convert.ToBase64String(salt));
+
+                // Calculer le hachage
+                byte[] hashedInputPassword = sha.ComputeHash(combined);
+
+                // Comparer le hachage calculé avec le hachage stocké
+                return storedHash == Convert.ToBase64String(hashedInputPassword);
+            }
+        }
+
+        public static string EncryptPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            else
+            {
+                byte[] storePassword = ASCIIEncoding.ASCII.GetBytes(password);
+                string encryptedPassword = Convert.ToBase64String(storePassword);
+                return encryptedPassword;
+            }
+        }
+
+        public static string DecryptPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            else
+            {
+                byte[] encryptedPassword = Convert.FromBase64String(password);
+                string decryptedPassword = ASCIIEncoding.ASCII.GetString(encryptedPassword);
+                return decryptedPassword;
+            }
+        }
+
         public async Task<RedirectResult> OnGetLogout()
         {
             await HttpContext.SignOutAsync();
--- pizza_mama/Pages/Admin/UsersManagement/Create.cshtml.cs
+++ pizza_mama/Pages/Admin/UsersManagement/Create.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +11,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using pizza_mama.Data;
 using pizza_mama_V2.Models;
+using pizza_mama.Pages.Admin;
+
 
 namespace pizza_mama_V2.Pages.Admin.UsersManagement
 {
@@ -25,16 +29,28 @@ namespace pizza_mama_V2.Pages.Admin.UsersManagement
         public IActionResult OnGet()
         {
             
-            if (!User.IsInRole("Administrator"))
-            {
-                // Redirection vers la page "Index" si l'utilisateur n'a pas le rôle "Admin"
-                return RedirectToPage("/Admin/UsersManagement/Index");
-            }
+            //if (!User.IsInRole("Administrator"))
+            //{
+            //    // Redirection vers la page "Index" si l'utilisateur n'a pas le rôle "Admin"
+            //    return RedirectToPage("/Admin/UsersManagement/Index");
+            //}
             return Page();
         }
-      
 
-    [BindProperty]
+        public static string EncryptPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+            {
+                return null;
+            }
+            else
+            {
+                byte[] storePassword = ASCIIEncoding.ASCII.GetBytes(password);
+                string encryptedPassword = Convert.ToBase64String(storePassword);
+                return encryptedPassword;
+            }
+        }
+        [BindProperty]
         public Account Account { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
@@ -45,10 +61,50 @@ namespace pizza_mama_V2.Pages.Admin.UsersManagement
                 return Page();
             }
 
+            //  Account.Password = HashPassword(Account.Password);
+
+            //     Account.Password = EncryptPassword(Account.Password);
+
+            //Account.Password = pizza_mama.Pages.Admin.IndexModel.EncryptPassword(Account.Password);
+
+            Account.Password = EncryptPassword(Account.Password);
+
+
+
             _context.Accounts.Add(Account);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
         }
+
+
+        private string HashPassword(string password)
+        {
+            using (var sha = SHA256.Create())
+            {
+                // Générer un sel aléatoire
+                byte[] salt = GenerateSalt();
+
+                // Concaténer le mot de passe avec le sel
+                byte[] combined = Encoding.UTF8.GetBytes(password + Convert.ToBase64String(salt));
+
+                // Calculer le hachage
+                byte[] hashedPassword = sha.ComputeHash(combined);
+
+                // Retourner le résultat haché
+                return Convert.ToBase64String(hashedPassword);
+            }
+        }
+
+        private byte[] GenerateSalt()
+        {
+            byte[] salt = new byte[16];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+
     }
 }
